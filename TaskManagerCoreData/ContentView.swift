533f4ad3006caef36386fbc956c0df81c1144012
Joel Lacerda <joellacerdaol@gmail.com>
2023-10-20 16:16:12 -0300

    Initial Commit

---
+++ TaskManagerCoreData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManagerCoreData
+//
+//  Created by Joel Lacerda on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagerCoreData/TaskManagerCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerCoreDataApp.swift
+//  TaskManagerCoreData
+//
+//  Created by Joel Lacerda on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
